192cf5611646abcaee75d9de75d8d6b838d9928a
UtkarshSahu9906 <UtkarshSahu9906@gmail.com>
2026-04-09 09:54:42 +0530

    parse config file

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/UtkarshSahu9906/students-api/inernal/config"
+)
 
 func main() {
-	fmt.Println("Hello, Students API!")
+	cfg := config.MustLoad()
+
+	 router :=http.NewServeMux()
+
+	 router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		//w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello, Students API!"))
+	})
+	server := &http.Server{
+		Addr: cfg.HTTPServer.Addr,
+		Handler: router,
+	}
+		fmt.Printf("Server is running on %s\n", cfg.HTTPServer.Addr)
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Failed to start server")
+	}
+
+
 }
